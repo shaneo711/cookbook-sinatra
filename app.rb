51e2f6d17849d9fcca4c686dd39eb4b3b1b62acd
Shane Garland <shane.garland1@gmail.com>
2022-01-24 12:35:00 +1100

    implemented code to create recipe

--- app.rb
+++ app.rb
@@ -9,6 +9,19 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+
+
 get '/' do
   erb :index
+end
+
+get '/new' do
+  erb :create_recipe
+end
+
+post '/recipes' do
+  cookbook = Cookbook.new("recipes.csv")
+  recipe = Recipe.new(params["name"], params["desc"], params["rating"], params["prep"])
+  cookbook.add_recipe(recipe)
+  redirect '/'
 end
